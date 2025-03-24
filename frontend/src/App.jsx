0c11471629896fffe6229e57b9bdc34a0e756381
Praveen Kumar <Praveenkumar042023@gmail.com>
2025-03-24 23:55:17 +0530

    Add signup,login,logout,update profile using axios to connect with backend APIs. Use DaisyUI for frontend components for Navbar,SignUp , login ,logout,profile.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,21 +6,44 @@ import LoginPage from "./pages/LoginPage.jsx"
 import SettingsPage from "./pages/SettingsPage.jsx"
 import SignUpPage from "./pages/SignUpPage.jsx"
 import ProfilePage from "./pages/ProfilePage.jsx"
-
+import { useAuthStore } from "./store/useAuthStore.js"
+import { useEffect } from "react"
+import {Home, Loader} from "lucide-react"
+import { Toaster } from "react-hot-toast"
 
 export default function App() {
+
+  const {authUser , checkAuth , isCheckingAuth} = useAuthStore();
+
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth]);
+
+  console.log({authUser}); 
+
+
+  if(!authUser && isCheckingAuth ){
+
+    return (
+      <div className="flex items-center justify-center h-screen">
+      <Loader className = "size-10 animate-spin"/>
+    </div>
+    )
+
+  }
   return (
     <>
       <Navbar/>
 
       <Routes>
-       <Route path="/" element = {<HomePage/>}/>  
+       <Route path="/" element = {authUser ? <HomePage/> : <LoginPage/>}/>  
        <Route path="/settings" element = {<SettingsPage/>}/>  
-       <Route path="/signup" element = {<SignUpPage/>}/>  
-       <Route path="/login" element = {<LoginPage/>}/>  
-       <Route path="/profile" element = {<ProfilePage/>}/>  
+       <Route path="/signup" element = {!authUser ? <SignUpPage/> : <HomePage/>}/>  
+       <Route path="/login" element = {!authUser ? <LoginPage/> : <HomePage/>}/>  
+       <Route path="/profile" element = {authUser ? <ProfilePage/> : <LoginPage/>}/>  
       </Routes>
 
+      <Toaster/>
     </>
   )
 }
