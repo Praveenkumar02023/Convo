7f4f0ccc05debf47964021405b1fd31657b9ca43
Praveen Kumar <Praveenkumar042023@gmail.com>
2025-03-26 01:56:11 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
  
-export const connetDB = async () =>{
+export const connectDB = async () =>{
 
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI);
