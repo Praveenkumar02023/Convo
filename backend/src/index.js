cde1b787165dbc404ca77058d5b64243cad205b6
Praveen Kumar <Praveenkumar042023@gmail.com>
2025-03-26 01:49:12 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,49 +1,42 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import { connetDB } from "./lib/db.js"
 import cors from "cors";
-import {app,server} from "./lib/socket.js"
+
 import path from "path";
 
-dotenv.config()
+import { connectDB } from "./lib/db.js";
 
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
+
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
-app.use(express.json())
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"]; // Add all possible frontend origins
-
+app.use(express.json());
+app.use(cookieParser());
 app.use(
-    cors({
-        origin: (origin, callback) => {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        },
-        credentials: true, // Allow cookies
-    })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 
-app.use(cookieParser());
-
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-  }
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-const PORT = process.env.PORT
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
-server.listen(PORT,()=>{
-    console.log("server running on port: " + PORT )
-    connetDB();
-})
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
